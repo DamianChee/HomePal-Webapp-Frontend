20399203f98b8abfd2fef2cd708540f30bc32d45
Damian Chee <silverousmechs@gmail.com>
2025-03-05 20:43:14 +0800

    Update EventTimeline.js

--- src/components/EventTimeline.js
+++ src/components/EventTimeline.js
@@ -112,7 +112,7 @@ function EventTimeline({
                 return false;
               })
               // Sort by timestamp (most recent first)
-              .sort((a, b) => parseTime(a.time) - parseTime(b.time));
+              .sort((a, b) => parseTime(b.time) - parseTime(a.time));
 
             // Skip rendering if no events to show
             if (filteredEvents.length === 0) return null;
