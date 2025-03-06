c9d62634e2f18251548a2e6f611d0289251b44b7
Damian Chee <silverousmechs@gmail.com>
2025-03-06 19:03:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,6 @@ import "./index.css";
 
 console.log("[FRONTEND] Starting application initialization...");
 
-// Verify Firebase client SDK
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    console.log("[FRONTEND] User logged in:", user.email);
-  } else {
-    console.log("[FRONTEND] No user logged in");
-  }
-});
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
