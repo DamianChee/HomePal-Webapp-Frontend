90960f85795f847c951c8a10a4224cc892b9d634
Damian Chee <silverousmechs@gmail.com>
2025-03-10 18:59:30 +0800

    Update EventTimeline.js

--- src/components/EventTimeline.js
+++ src/components/EventTimeline.js
@@ -94,10 +94,11 @@ function EventTimeline({
               const today = new Date();
               const yesterday = new Date(today);
               yesterday.setDate(yesterday.getDate() - 1);
+              console.log(d);
+              console.log(today);
 
-              if (d.toDateString() === today.toDateString()) return "Today";
-              if (d.toDateString() === yesterday.toDateString())
-                return "Yesterday";
+              if (d === today.toDateString()) return "Today";
+              if (d === yesterday.toDateString()) return "Yesterday";
               return d.toLocaleDateString(); // Fallback for other dates
             };
 
