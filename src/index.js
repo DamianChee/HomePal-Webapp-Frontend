dd1084af514fd24d33cc2a852f028839f48ab38a
Damian Chee <silverousmechs@gmail.com>
2025-03-10 21:32:04 +0800

    Claude Attempt to fix mobile

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { auth } from "./firebase";
-import App from "./App";
-import "./index.css";
-
-console.log("[FRONTEND] Starting application initialization...");
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { app } from "./firebase";
+import App from "./App";
+import "./index.css";
+
+console.log("[FRONTEND] Starting application initialization...");
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
