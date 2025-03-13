f1a38e341b5432fc599c6e4ee68eb1149498948a
Damian Chee <silverousmechs@gmail.com>
2025-03-13 20:23:44 +0800

    Update EventDetail.js

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -39,22 +39,25 @@ function EventDetail({ event, getStatusColor, onClose }) {
         return (
           <Slider {...settings}>
             <div>
-              <h3>1</h3>
+              <img
+                src="/images/attempted-bedside-exit.jpg"
+                alt="Static image from public folder"
+                style={{ display: `inline`, height: `-webkit-fill-available` }}
+              />
             </div>
             <div>
-              <h3>2</h3>
+              <img
+                src="/images/attempted-bedside-exit.jpg"
+                alt="Static image from public folder"
+                style={{ display: `inline`, height: `-webkit-fill-available` }}
+              />
             </div>
             <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
+              <img
+                src="/images/attempted-bedside-exit.jpg"
+                alt="Static image from public folder"
+                style={{ display: `inline`, height: `-webkit-fill-available` }}
+              />
             </div>
           </Slider>
         );
