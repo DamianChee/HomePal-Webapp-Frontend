e56f6e47203409c0cba622c1c08c0f8037c7c6bd
Damian Chee <silverousmechs@gmail.com>
2025-03-13 19:48:51 +0800

    Update EventDetail.js

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -45,43 +45,43 @@ function EventDetail({ event, getStatusColor, onClose }) {
         );
       case "Bed-Exit":
         return (
-          <div class="snap-x">
-            <div class="snap-center">
+          <div class="snap-x ...">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
                 style={{ display: `inline`, height: `-webkit-fill-available` }}
               />
             </div>
-            <div class="snap-center">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
                 style={{ display: `inline`, height: `-webkit-fill-available` }}
               />
             </div>
-            <div class="snap-center">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
                 style={{ display: `inline`, height: `-webkit-fill-available` }}
               />
             </div>
-            <div class="snap-center">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
                 style={{ display: `inline`, height: `-webkit-fill-available` }}
               />
             </div>
-            <div class="snap-center">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
                 style={{ display: `inline`, height: `-webkit-fill-available` }}
               />
             </div>
-            <div class="snap-center">
+            <div class="snap-center ...">
               <img
                 src="/images/bed-exit.jpg"
                 alt="Static image from public folder"
