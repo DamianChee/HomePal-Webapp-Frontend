75959de3f171159f110bebf07a42ee4a48bd648b
Damian Chee <silverousmechs@gmail.com>
2025-03-13 21:06:27 +0800

    test 6

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -3,7 +3,7 @@ import { X } from "lucide-react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import "../custom-slick.css";
+import "../custom-slick.css";
 
 /**
  * EventDetail Component
